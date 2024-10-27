5029905d7f2c84c9861274688ce1dfe7d65962c6
Vikrant kumar <151379848+vikrant-kumar-cse@users.noreply.github.com>
2024-10-27 16:43:47 +0530

    nvxz

--- Codewithvikrant/settings.py
+++ Codewithvikrant/settings.py
@@ -174,8 +174,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL="/media/"
 MEDIA_ROOT= BASE_DIR
-STATICFILES_DIRS=[BASE_DIR/'static']
-STATIC_ROOT=BASE_DIR / "staticfiles"
 
 
 # Enable WhiteNoise compression
