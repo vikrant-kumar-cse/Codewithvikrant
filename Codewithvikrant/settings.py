b5fac1bd22f9cd3275a5921dfd943bc395542cf8
Vikrant kumar <151379848+vikrant-kumar-cse@users.noreply.github.com>
2024-10-27 17:05:19 +0530

    kjfh

--- Codewithvikrant/settings.py
+++ Codewithvikrant/settings.py
@@ -172,8 +172,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL="/media/"
-MEDIA_ROOT= BASE_DIR
+#MEDIA_URL="/media/"
+#MEDIA_ROOT= BASE_DIR
+STATICFILES_DIRS=[BASE_DIR / 'static']
+STATIC_ROOT=BASE_DIR / "staticfiles"
 
 
 # Enable WhiteNoise compression
