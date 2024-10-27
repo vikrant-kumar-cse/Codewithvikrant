1cb9c12fd58899ffbb4386b66f815b529ed5c2d7
Vikrant kumar <151379848+vikrant-kumar-cse@users.noreply.github.com>
2024-10-27 12:52:23 +0530

    Update settings.py

--- Codewithvikrant/settings.py
+++ Codewithvikrant/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = 'django-insecure-2*)uzmcfb*dib6#b%c#!y)7snx9te#lo3e3n(%i#7ofkrqsnje
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
